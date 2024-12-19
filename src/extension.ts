e0a06bf8f5a91cda2a8cbecdcae6653ea146e76e
bamps53 <daisuke0530@gmail.com>
2024-12-20 07:28:18 +0900

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "llm-clipboard.copy",
+    async (resource: vscode.Uri) => {
+      if (!resource) {
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "llm-clipboard" is now active!');
+      const fileName = path.basename(resource.fsPath);
+      const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('llm-clipboard.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from llm_clipboard!');
-	});
+      if (editor) {
+        editor.edit((editBuilder) => {
+          editBuilder.replace(
+            editor.selection,
+            fileName + "\n" + "${clipboard}"
+          );
+        });
+      } else {
+        // If there is no active editor, copy to clipboard
+        await vscode.env.clipboard.writeText(fileName + "\n" + "${clipboard}");
+        vscode.window.showInformationMessage(
+          `Copied file name "${fileName}" to clipboard.`
+        );
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
